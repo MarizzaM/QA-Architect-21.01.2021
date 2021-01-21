26da70e9678fa9807e87e7840a2a537e582837f0
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-01-21 20:17:17 +0200

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Dog {
+    public String name;
+    public String color;
+    public float height;
+    public String brand;
+
+    public void bark(){
+        System.out.println("barking");
+    }
+
+    public void jump(){
+        System.out.println("jumping");
+    }
+
+    public void sayYourName(){
+        System.out.println("my name " + name);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog Rexi = new Dog();
+        Rexi.name = "Rexi";
+        Rexi.color = "brown";
+        Rexi.height = 0.57f;
+        Rexi.brand = "wolf";
+        System.out.println(Rexi.name);
+        System.out.println(Rexi.color);
+        System.out.println(Rexi.height);
+        System.out.println(Rexi.brand);
+        Rexi.bark();
+        Rexi.jump();
+        Rexi.sayYourName();
+
+        Dog Socks = new Dog();
+        Socks.name = "Socks";
+        Socks.color = "white";
+        Socks.height = 0.4f;
+        Socks.brand = "chiwawa";
+        System.out.println(Socks.name);
+        System.out.println(Socks.color);
+        System.out.println(Socks.height);
+        System.out.println(Socks.brand);
+        Socks.bark();
+        Socks.jump();
+        Socks.sayYourName();
+
+
+
+
+
+
+
+
+
+
+    }
+}
